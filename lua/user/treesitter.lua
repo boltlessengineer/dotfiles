1fdb823c52822c9f50909bfcb70f70ef0c1a6e40
Seongmin Lee <boltlessengineer@gmail.com>
2022-01-05 15:19:18 +0900

    Autopairs X TreeSitter

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -2,9 +2,12 @@ local ok, tsconfigs = pcall(require, "nvim-treesitter.configs")
 if not ok then return end
 
 tsconfigs.setup {
-  ensure_installed = "maintained", -- "all" | "maintained"
+  ensure_installed = "maintained", -- "all" | "maintained" | a list of languages
   sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
   ignore_install = { "" }, -- List of parsers to ignore installing
+  autopairs = {
+    enable = true,
+  },
   highlight = {
     enable = true, -- false will disable the whole extension
     disable = { "" }, -- list of language that will be diagnostic_disabled
