3eeedb7415375b9947cc63e9fc61a9eaa093e69c
Seongmin Lee <boltlessengineer@gmail.com>
2022-04-16 15:16:21 +0900

    Fix double definitions error in sumneko_lua

--- lua/user/lsp/settings/sumneko_lua.lua
+++ lua/user/lsp/settings/sumneko_lua.lua
@@ -7,7 +7,7 @@ return {
       workspace = {
         library = {
           [vim.fn.expand("$VIMRUNTIME/lua")] = true,
-          [vim.fn.stdpath("config") .. "/lua"] = true,
+          -- [vim.fn.stdpath("config") .. "/lua"] = true,
         },
       },
     },
