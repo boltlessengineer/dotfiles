50a77fe86c86585da4c5a469c69c02a0f1db6e99
BoltlessEngineer <boltlessengineer@gmail.com>
2022-04-29 14:25:42 +0900

    Fix nvim-tree setup option changes (not done yet)

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -33,8 +33,10 @@ end
 
 local tree_cb = nvim_tree_config.nvim_tree_callback
 
+-- TODO: Update this with original github setup {} code
 nvim_tree.setup {
   disable_netrw = true,
+  hijack_cursor = false,
   hijack_netrw = true,
   open_on_setup = false,
   ignore_ft_on_setup = {
@@ -43,11 +45,22 @@ nvim_tree.setup {
     "alpha",
   },
   open_on_tab = false,
-  hijack_cursor = false,
   update_cwd = true,
-  update_to_buf_dir = {
-    enable = true,
-    auto_open = true,
+  view = {
+    width = 30,
+    height = 30,
+    hide_root_folder = false,
+    side = "left",
+    mappings = {
+      custom_only = false,
+      list = {
+        { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
+        { key = "h", cb = tree_cb "close_node" },
+        { key = "v", cb = tree_cb "vsplit" },
+      },
+    },
+    number = false,
+    relativenumber = false,
   },
   diagnostics = {
     enable = true,
@@ -74,39 +87,11 @@ nvim_tree.setup {
   git = {
     enable = true,
     ignore = true,
-    timeout = 500,
-  },
-  view = {
-    width = 30,
-    height = 30,
-    hide_root_folder = false,
-    side = "left",
-    auto_resize = true,
-    mappings = {
-      custom_only = false,
-      list = {
-        { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
-        { key = "h", cb = tree_cb "close_node" },
-        { key = "v", cb = tree_cb "vsplit" },
-      },
-    },
-    number = false,
-    relativenumber = false,
+    timeout = 400,
   },
   trash = {
     cmd = "trash",
     require_confirm = true,
-  },
-  quit_on_open = 0,
-  git_hl = 1,
-  disable_window_picker = 0,
-  root_folder_modifier = ":t",
-  show_icons = {
-    git = 1,
-    folders = 1,
-    files = 1,
-    folder_arrows = 1,
-    tree_width = 30,
   },
   actions = {
     open_file = {
