6a9c3f3bc86de6500d008e25114c1c40365c15e2
BoltlessEngineer <boltlessengineer@gmail.com>
2022-04-29 14:25:07 +0900

    numToStr/Comment.nvim only works in Neovim v0.7+

--- lua/user/comment.lua
+++ lua/user/comment.lua
@@ -7,21 +7,23 @@ comment.setup {
     extra = false,
     extended = false,
   },
-  pre_hook = function (ctx)
-    local U = require "Comment.utils"
+  pre_hook = function(ctx)
+    if vim.bo.filetype == 'typescriptreact' then
+      local U = require "Comment.utils"
 
-    local location = nil
-    if ctx.ctype == U.ctype.block then
-      location = require("ts_context_commentstring.utils").get_cursor_location()
-    elseif ctx.motion == U.cmotion.v or ctx.motion == U.cmotion.V then
-      location = require("ts_context_commentstring.utils").get_visual_start_location()
+      local location = nil
+      if ctx.ctype == U.ctype.block then
+        location = require("ts_context_commentstring.utils").get_cursor_location()
+      elseif ctx.motion == U.cmotion.v or ctx.motion == U.cmotion.V then
+        location = require("ts_context_commentstring.utils").get_visual_start_location()
+      end
+
+      return require("ts_context_commentstring.internal").calculate_commentstring {
+        -- Determine whether to use linewise or blockwise commentstring
+        key = ctx.ctype == U.ctype.line and "__default" or "__multiline",
+        location = location,
+      }
     end
-
-    return require("ts_context_commentstring.internal").calculate_commentstring {
-      -- Determine whether to use linewise or blockwise commentstring
-      key = ctx.ctype == U.ctype.line and "__default" or "__multiline",
-      location = location,
-    }
   end
 }
 
