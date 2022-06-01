7db3cbf0ac6aaf9173cedf865fb382353f9c7b86
BoltlessEngineer <boltlessengineer@gmail.com>
2022-06-01 19:48:45 +0900

    [nvim] smart indent-blankline config

--- nvim/lua/plugins/indentblankline.lua
+++ nvim/lua/plugins/indentblankline.lua
@@ -1,10 +1,8 @@
 local ok, indent = pcall(require, "indent_blankline")
-if not ok then
-  vim.notify("no indent_backline")
-  return
-end
+if not ok then return end
 
 indent.setup {
+  -- Hide indent-blankline if tap-width is 2
+  char = (vim.opt.shiftwidth:get() == 4) and "│" or "",
   show_current_context = true,
-  -- show_current_context_start = true,
 }
