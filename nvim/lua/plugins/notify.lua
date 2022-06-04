335afd376a32b1177b4985c029891d6d8b3ce0dc
BoltlessEngineer <boltlessengineer@gmail.com>
2022-06-05 00:14:02 +0900

    [nvim] fix: new property for notify-render()

--- nvim/lua/plugins/notify.lua
+++ nvim/lua/plugins/notify.lua
@@ -6,15 +6,26 @@ end
 local renderers = require("notify.render")
 
 notify.setup {
-  stages = 'slide',
-  timeout = 2500,
-  render = function (bufnr, notif, highl)
+  background_colour = "Normal",
+  fps = 30,
+  icons = {
+    DEBUG = "",
+    ERROR = "",
+    INFO = "",
+    TRACE = "✎",
+    WARN = ""
+  },
+  level = 2,
+  minimum_width = 50,
+  render = function(bufnr, notif, highlights, config)
     if notif.title[1] == "" then
-      return renderers.minimal(bufnr, notif, highl)
+      return renderers.minimal(bufnr, notif, highlights, config)
     else
-      return renderers.default(bufnr, notif, highl)
+      return renderers.default(bufnr, notif, highlights, config)
     end
-  end
+  end,
+  stages = "fade_in_slide_out",
+  timeout = 2000,
 }
 
 vim.notify = notify
