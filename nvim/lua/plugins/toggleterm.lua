410f48745fba974cf16c481e958044fca54f24b7
BoltlessEngineer <boltlessengineer@gmail.com>
2022-06-06 14:35:06 +0900

    [nvim] add: keymaps for Toggleterms (except lazygit)

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -11,6 +11,9 @@ function _Lazygit_toggle()
     hidden = true,
     dir = "git_dir",
     direction = "float",
+    on_open = function()
+      -- Don't use general on_open function defined below
+    end
   })
   lazygit:toggle()
 end
@@ -24,4 +27,10 @@ toggleterm.setup {
     end
   end,
   open_mapping = [[<c-t>]],
+  on_open = function(term)
+    local opts = { buffer = term.bufnr }
+    -- Fast enter NORMAL mode
+    vim.keymap.set("t", "<ESC>", [[<C-\><C-n>]], opts)
+    vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
+  end,
 }
