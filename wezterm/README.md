3330e70f27518ff2817c41349f6cac26221b42a4
BoltlessEngineer <boltlessengineer@gmail.com>
2022-11-25 05:37:55 +0900

    add fancy wezterm icon

---
+++ wezterm/README.md
@@ -0,0 +1,3 @@
+# Wezterm terminal config
+
+App icon is from [wezterm#2396](https://github.com/wez/wezterm/discussions/2396)
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -2,6 +2,7 @@ local wezterm = require 'wezterm'
 return {
   font = wezterm.font_with_fallback {
     'Fira Code',
+    'Menlo',
     'Symbols Nerd Font',
     -- TODO: better Korean font
   },
