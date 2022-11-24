d187e07fe8052de993408b6bd64a427c0e1fe7d8
BoltlessEngineer <boltlessengineer@gmail.com>
2022-11-25 04:13:55 +0900

    modified some font settings in wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,9 +1,10 @@
 local wezterm = require 'wezterm'
 return {
   font = wezterm.font_with_fallback {
-    'FiraCode Nerd Font',
+    'Fira Code',
+    'Symbols Nerd Font',
     -- TODO: better Korean font
   },
+  font_size = 16.0,
   color_scheme = 'Catppuccin Mocha',
-  window_background_opacity = 0.97,
 }
