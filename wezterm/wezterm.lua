4394fe4cfada17b74bd8d3b9c741851b17a86906
BoltlessEngineer <boltlessengineer@gmail.com>
2023-01-19 23:15:13 +0900

    feat(wezterm): Tmux-like tabbar (wip)

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,4 +1,83 @@
 local wezterm = require 'wezterm'
+
+local function get_process(tab)
+  local process_icons = {
+    ["fish"] = {
+      { Text = wezterm.nerdfonts.mdi_fish },
+    },
+    ["docker"] = {
+      { Text = wezterm.nerdfonts.linux_docker },
+    },
+    ["nvim"] = {
+      { Text = wezterm.nerdfonts.custom_vim },
+    },
+    ["vim"] = {
+      { Text = wezterm.nerdfonts.dev_vim },
+    },
+    ["node"] = {
+      { Text = wezterm.nerdfonts.mdi_hexagon },
+    },
+    ["go"] = {
+      { Text = wezterm.nerdfonts.mdi_language_go },
+    },
+    ["cargo"] = {
+      { Text = wezterm.nerdfonts.dev_rust },
+    },
+    ["lua"] = {
+      { Text = wezterm.nerdfonts.seti_lua },
+    },
+    ["wget"] = {
+      { Text = wezterm.nerdfonts.mdi_arrow_down_box },
+    },
+    ["curl"] = {
+      { Text = wezterm.nerdfonts.mdi_flattr },
+    },
+    ['lazygit'] = {
+      { Text = wezterm.nerdfonts.dev_git },
+    },
+  }
+
+  local process_name = string.gsub(tab.active_pane.foreground_process_name, "(.*[/\\])(.*)", "%2")
+
+  return wezterm.format(process_icons[process_name]
+    or {
+      { Text = string.format('[%s]', process_name) }
+    })
+end
+
+local function get_current_working_dir(tab)
+  local current_dir = tab.active_pane.current_working_dir
+  local HOME_DIR = string.format("file://%s", os.getenv("HOME"))
+  return current_dir == HOME_DIR and "  ~"
+      or string.format("  %s", string.gsub(current_dir, "(.*[/\\])(.*)", "%2"))
+end
+
+wezterm.on(
+  'format-tab-title',
+  function(tab, _tabs, _panes, _config, hover, _max_width)
+    local background = '#1b1032'
+    local foreground = '#c0c0c0'
+    if tab.is_active then
+      local tmp = background
+      background = foreground
+      foreground = tmp
+    elseif hover then
+      background = '#3b3052'
+      foreground = '#909090'
+    end
+
+    return wezterm.format({
+      { Foreground = { Color = foreground } },
+      { Background = { Color = background } },
+      { Text = string.format(' %s  ', tab.tab_index + 1) },
+      { Text = get_process(tab) },
+      { Text = " " },
+      { Text = get_current_working_dir(tab) },
+      { Text = ' ▕' },
+    })
+  end
+)
+
 return {
   font = wezterm.font_with_fallback {
     'Fira Code',
@@ -37,7 +116,10 @@ return {
   },
   line_height = 1.2,
   allow_square_glyphs_to_overflow_width = "Always",
-  hide_tab_bar_if_only_one_tab = true,
+  hide_tab_bar_if_only_one_tab = false,
+  use_fancy_tab_bar = false,
+  tab_bar_at_bottom = true,
+  tab_max_width = 50,
   adjust_window_size_when_changing_font_size = false,
   window_padding = {
     top    = 0,
