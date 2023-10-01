1e809073c4df71ce44abf41ea986162d35191eaa
BoltlessEngineer <boltlessengineer@gmail.com>
2023-10-01 23:31:43 +0900

    style(fish): format code with `fish_indent`

--- fish/config.fish
+++ fish/config.fish
@@ -1,52 +1,52 @@
 if status is-interactive
-  # Commands to run in interactive sessions can go here
-  starship init fish | source
-  zoxide init fish | source
+    # Commands to run in interactive sessions can go here
+    starship init fish | source
+    zoxide init fish | source
 
-  # Aliases
-  alias l="exa"
-  alias lt="exa -T"
-  alias lT="exa -Tlh --no-user --no-time"
-  alias ll="exa -lh --no-user"
-  alias la="exa -lha --git --no-user"
-  alias nv="foreground_nvim"
-  alias gr="gh repo clone"
-  alias glz="lazygit"
+    # Aliases
+    alias l="exa"
+    alias lt="exa -T"
+    alias lT="exa -Tlh --no-user --no-time"
+    alias ll="exa -lh --no-user"
+    alias la="exa -lha --git --no-user"
+    alias nv="foreground_nvim"
+    alias gr="gh repo clone"
+    alias glz="lazygit"
 
-  # Exported Variables
-  set -gx PATH ~/.local/bin $PATH
+    # Exported Variables
+    set -gx PATH ~/.local/bin $PATH
 
-  if not set -q XDG_CONFIG_HOME
-    set -gx XDG_CONFIG_HOME "$HOME/.config"
-  end
+    if not set -q XDG_CONFIG_HOME
+        set -gx XDG_CONFIG_HOME "$HOME/.config"
+    end
 
-  set -gx EDITOR nvim
+    set -gx EDITOR nvim
 
-  # set $TERM as wezterm to support undercurl in neovim (see wezterm#2505)
-  if test "$TERM_PROGRAM" = "WezTerm"
-    set -gx TERM wezterm
-  end
+    # set $TERM as wezterm to support undercurl in neovim (see wezterm#2505)
+    if test "$TERM_PROGRAM" = WezTerm
+        set -gx TERM wezterm
+    end
 
-  ## Go
-  set -gx GOROOT "$(brew --prefix golang)/libexec"
-  set -gx GOPATH $HOME/go
-  set -gx PATH $PATH $GOROOT/bin $GOPATH/bin
+    ## Go
+    set -gx GOROOT "$(brew --prefix golang)/libexec"
+    set -gx GOPATH $HOME/go
+    set -gx PATH $PATH $GOROOT/bin $GOPATH/bin
 
-  ## Flutter
-  set -gx PATH $PATH /opt/homebrew/Caskroom/flutter/2.10.4/flutter/bin
+    ## Flutter
+    set -gx PATH $PATH /opt/homebrew/Caskroom/flutter/2.10.4/flutter/bin
 
-  ## Rust
-  set -gx PATH $PATH $HOME/.cargo/bin
+    ## Rust
+    set -gx PATH $PATH $HOME/.cargo/bin
 
-  ## FZF
-  set -gx FZF_DEFAULT_OPTS "--bind='ctrl-u:preview-half-page-up' \
+    ## FZF
+    set -gx FZF_DEFAULT_OPTS "--bind='ctrl-u:preview-half-page-up' \
 --bind='ctrl-d:preview-half-page-down'"
 
-  # brew shell completion (see: https://docs.brew.sh/Shell-Completion)
-  if test -d (brew --prefix)"/share/fish/completions"
-      set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/completions
-  end
-  if test -d (brew --prefix)"/share/fish/vendor_completions.d"
-      set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
-  end
+    # brew shell completion (see: https://docs.brew.sh/Shell-Completion)
+    if test -d (brew --prefix)"/share/fish/completions"
+        set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/completions
+    end
+    if test -d (brew --prefix)"/share/fish/vendor_completions.d"
+        set -gx fish_complete_path $fish_complete_path (brew --prefix)/share/fish/vendor_completions.d
+    end
 end
