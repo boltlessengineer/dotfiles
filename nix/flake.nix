f76115b6948bbe2924ea2117ca516c6d23235b5b
Seongmin Lee <boltlessengineer@gmail.com>
2025-04-26 16:59:37 +0000

    feat(nix): add home-manager

--- nix/flake.nix
+++ nix/flake.nix
@@ -5,11 +5,11 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-    # home-manager.url = "github:nix-community/home-manager/release-24.05";
-    # home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager/release-24.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs }:
+  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
   let
     configuration = { pkgs, ... }: {
       # List packages installed in system profile. To search by name, run:
@@ -48,6 +48,10 @@
         # home-manager.darwinModules.home-manager
         configuration
       ];
+    };
+    homeConfigurations."ubuntu" = home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages."aarch64-linux";
+      modules = [ ./home.nix ];
     };
   };
 }
