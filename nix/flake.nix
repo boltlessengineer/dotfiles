33f9491e8100b9518178a1d08a1c667a38a5e44c
Seongmin Lee <boltlessengineer@proton.me>
2025-05-22 17:57:15 +0900

    feat(nix): proper nix-darwin setup with new MBA

--- nix/flake.nix
+++ nix/flake.nix
@@ -1,57 +1,54 @@
 {
-  description = "Example nix-darwin system flake";
+  description = "nix-darwin system flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    nix-darwin.url = "github:LnL7/nix-darwin/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.11-darwin";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
+    nix-darwin.url = "github:nix-darwin/nix-darwin/nix-darwin-24.11";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-    home-manager.url = "github:nix-community/home-manager/release-24.11";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = inputs@{ self, nix-darwin, nixpkgs, home-manager }:
+  outputs = inputs@{
+    self,
+    nix-darwin,
+    nixpkgs,
+    nixpkgs-unstable
+  }:
   let
-    configuration = { pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
-      environment.systemPackages =
-        with pkgs; [
-          vim
-          typescript-language-server
-          (pkgs.lua5_1.withPackages (ps: with ps; [luarocks]))
-        ];
-
-      # Necessary for using flakes on this system.
-      nix.settings.experimental-features = "nix-command flakes";
-      nix.settings.extra-platforms = "x86_64-darwin aarch64-darwin";
-
-      # Enable alternative shell support in nix-darwin.
-      # programs.fish.enable = true;
-
-      # Set Git commit hash for darwin-version.
-      system.configurationRevision = self.rev or self.dirtyRev or null;
-
-      # Used for backwards compatibility, please read the changelog before changing.
-      # $ darwin-rebuild changelog
-      system.stateVersion = 6;
-
-      # The platform the configuration will be used on.
-      nixpkgs.hostPlatform = "aarch64-darwin";
-    };
+    system = "aarch64-darwin";
+    pkgs = nixpkgs.legacyPackages.${system};
+    pkgs-unstable = nixpkgs-unstable.legacyPackages.${system};
   in
   {
     # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#simple
-    darwinConfigurations."macbook" = nix-darwin.lib.darwinSystem {
-      # system = "aarch64-darwin";
+    # $ darwin-rebuild build --flake ~/.config/nix
+    # $ darwin-rebuild switch --flake ~/.config/nix
+    darwinConfigurations."boltless-MacBook-Air" = nix-darwin.lib.darwinSystem {
       modules = [
-        # home-manager.darwinModules.home-manager
-        configuration
+        ./darwin.nix
       ];
+      specialArgs = {
+        inherit self pkgs-unstable;
+      };
     };
-    homeConfigurations."ubuntu" = home-manager.lib.homeManagerConfiguration {
-      pkgs = nixpkgs.legacyPackages."aarch64-linux";
-      modules = [ ./home.nix ];
+    # based on https://zaynetro.com/post/2024-you-dont-need-home-manager-nix
+    # $ nix profile install .
+    # $ nix profile upgrade nix
+    packages.${system}.default = pkgs.buildEnv {
+      name = "my-env";
+      paths = [
+        pkgs.btop
+        pkgs.gh
+        pkgs.hello
+        pkgs.janet
+        pkgs.meld
+        pkgs.sumneko-lua-language-server
+        pkgs.unixtools.watch
+        pkgs-unstable.jujutsu
+        pkgs-unstable.neovim
+      ];
     };
   };
 }
+# vim: et:ts=2
